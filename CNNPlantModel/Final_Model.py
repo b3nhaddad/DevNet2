1c30cdadc8926a8eb87399fb6a23dd860b9350f4
HJGates <157148033+HJGates@users.noreply.github.com>
2024-11-12 21:16:10 -0700

    Added finished model
    - Worker file is the full training that was done on colab
    - cnn_model_weights.pth is saved model weights which get loaded
    - Final model is usable python file

---
+++ CNNPlantModel/Final_Model.py
@@ -0,0 +1,27 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from PIL import Image
+import os
+import torch
+from torch.utils.data import Dataset
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import train_test_split
+import shutil
+from torchvision import transforms
+from tqdm import tqdm
+from torchvision import models
+
+
+CNN_model = models.resnet18(pretrained=False)
+num_features = CNN_model.fc.in_features
+CNN_model.fc = nn.Linear(num_features, 2)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+CNN_model = CNN_model.to(device)
+
+model_path = '/Users/henry/Desktop/CNNPlantModel/cnn_model_weights.pth'
+CNN_model.load_state_dict(torch.load(model_path, map_location=device))
+
+CNN_model.eval()
