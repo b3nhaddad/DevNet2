785a73703dfa0f3e792e2bb1e633eea9b308b405
b3nhaddad <98062395+b3nhaddad@users.noreply.github.com>
2024-11-12 17:51:54 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
-import torch.nn as nn # type: ignore
-import torch.nn.functional as F # type: ignore
-import torch.optim as optim # type: ignore
+import torch
+import torch.nn as nn 
+import torch.nn.functional as F  
+import torch.optim as optim 
+
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3,6,5)
-        self.pool1 = nn.MaxPool2d(2,2)
-        self.conv2 = nn.Conv2d(6,16, 5)
-        self.fc1 = nn.Linear(20, 520)
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 520)  # Adjust the input size to match the flattened output size
         self.fc2 = nn.Linear(520, 2)
 
-
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool1(F.relu(self.conv2(x)))
         x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
+
 
 CNN_model = Net()
 criterion = nn.CrossEntropyLoss()
