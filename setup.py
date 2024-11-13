8b213355bd8bcb5d3d54f34457ec779d09c09918
Chickerson64 <carterhickerson04@gmail.com>
2024-11-13 11:48:59 -0700

    Update setup.py Final

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from PIL import Image
+import os
 import torch
-import torch.nn as nn 
-import torch.nn.functional as F  
-import torch.optim as optim 
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool1 = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 520)  # Adjust the input size to match the flattened output size
-        self.fc2 = nn.Linear(520, 2)
-
-    def forward(self, x):
-        x = self.pool1(F.relu(self.conv1(x)))
-        x = self.pool1(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
+from torch.utils.data import Dataset
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import train_test_split
+import shutil
+from torchvision import transforms
+from tqdm import tqdm
+from torchvision import models
 
 
-CNN_model = Net()
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(CNN_model.parameters(), lr=0.01)
+CNN_model = models.resnet18(weights=None)
+num_features = CNN_model.fc.in_features
+CNN_model.fc = nn.Linear(num_features, 2)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+CNN_model = CNN_model.to(device)
+
+model_path = '/s/bach/l/under/carter64/public_html/cnn_model_weights.pth'
+CNN_model.load_state_dict(torch.load(model_path, map_location=device))
+
+CNN_model.eval()
